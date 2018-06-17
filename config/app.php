82db70c3d730f70d06bee34d8210c2b617faf665
Kagami <490584832@qq.com>
2018-06-17 05:19:01 +0000

    add the return json method

--- config/app.php
+++ config/app.php
@@ -136,6 +136,17 @@ return [
     |
     */
 
+    /*
+    |--------------------------------------------------------------------------
+    | customized http code
+    |--------------------------------------------------------------------------
+    |
+    | The first number is error type, the second and third number is
+    | product type, and it is a specific error code from fourth to
+    | sixth.But the success is different.
+    |
+    */
+
     'providers' => [
 
         /*
