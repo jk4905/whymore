9275a2e873a31ff77a0b990cc4936442477660a4
Kagami <jk4905@126.com>
2018-09-10 22:23:10 +0800

    fix confirm order api

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['http://mytest.com'],
+    'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
